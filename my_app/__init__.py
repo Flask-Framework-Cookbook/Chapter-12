198d50e5557203af45394975bacb6fd3b94d719a
Shalabh Aggarwal <shalabh7777@gmail.com>
2014-10-27 16:41:52 +0530

    Chapter 12 - Recipe 'Email support for Flask applications'

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -2,12 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from elasticsearch import Elasticsearch
 from flask_caching import Cache
+from flask_mail import Mail
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['WHOOSH_BASE'] = '/tmp/whoosh'
 db = SQLAlchemy(app)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'gmail_username'
+app.config['MAIL_PASSWORD'] = 'gmail_password'
+app.config['MAIL_DEFAULT_SENDER'] = ('Sender name', 'sender email')
+mail = Mail(app)
 
 app.secret_key = 'some_random_key'
 
